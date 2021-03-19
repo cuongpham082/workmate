bdfad28997113e064352b27d1be0be2f4b0ee50a
CuongPham <pvcuong@tma.com.vn>
2021-03-19 15:05:52 +0700

    init code

---
+++ src/main/java/com/seerpharma/workmate/WorkmateApplication.java
@@ -0,0 +1,13 @@
+package com.seerpharma.workmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkmateApplication.class, args);
+	}
+
+}
